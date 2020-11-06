9133c10446b7fb28b78deab96e2e4610e4d06d86
Taga Studios <tagastudios@gmail.com>
2020-11-05 23:14:21 -0500

    Push inicial de la seccion 5

---
+++ js/laptop.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   const top = document.querySelectorAll('.top')
+
+//   top.classlist.remove('.top');
+//   top.classlist.add('.top2');
+// })
